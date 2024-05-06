8e67d427d0469a924ec0230de9c1a053936dff4d
Circle CI <circleci@logicblocks.io>
2024-05-06 18:50:12 +0000

    Pre-release version 0.2.0-RC19 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/spec.validate "0.2.0-RC18"
+(defproject io.logicblocks/spec.validate "0.2.0-RC19"
   :description "A clojure.spec based validation library."
   :url "https://github.com/logicblocks/spec.validate"
 
